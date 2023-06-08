497df00bb61c15b26d7e50da7e7bee6f9c591bfe
miguel-mancera997 <113144211+miguel-mancera997@users.noreply.github.com>
2023-06-08 12:23:45 -0700

    Changes: Added Spider-Man Movie Titles

--- app2.py
+++ app2.py
@@ -1,11 +1,15 @@
 # This code looks at index.html file and grabs the information from there and puts it on a webpage on localhost. 
 from flask import Flask, render_template
+from imdb import IMDb
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    ia = IMDb()
+    movies = ia.search_movie('Spider-Man')
+    return render_template('index.html', movies=movies)
 
 if __name__ == '__main__':
     app.run()
